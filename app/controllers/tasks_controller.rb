e34305f3a7e082f239682f3e9370f733f2f7a238
hrkmay <hrkmay8492@gmail.com>
2022-04-20 21:41:37 +0900

    タスクの追加機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,26 @@
 class TasksController < ApplicationController
+
   def index
-  @tasks = Task.all
+   @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+   @task = Task.create(task_params)
+   if @task.save
+    redirect_to root_path
+   else
+    @task = Task.new
+    render :new
+   end
+  end
+
+ private
+   def task_params
+    params.require(:task).permit(:title, :sentence, :year_id, :month_id, :day_id, :dayofweek_id, :weather_id)
+   end
+
 end
